1034b271450fd03a9f6d3845b086d25dbd37b956
yigit <yigit@pixelmilldigital.com>
2019-10-27 15:24:15 +0000

    New models for weapons. New raycast system for targets. Bullet muzzle flashes and particles added.

--- Assets/PlayerFire.cs
+++ Assets/PlayerFire.cs
@@ -6,24 +6,68 @@ public class PlayerFire : MonoBehaviour
 {
     public GameObject BulletPrefab;
     public float Bulletspeed = 5000;
-
+    public float Damage = 10f;
+    public float Range = 100f;
     public GameObject Location1;
+    public GameObject Location2;
 
     private Ray ray;
     private RaycastHit hit;
 
+    public ParticleSystem MuzzleFlash1;
+    public ParticleSystem MuzzleFlash2;
+
+    private GameObject Bullet1;
+    private GameObject Bullet2;
+
+    private void Start()
+    {
+        MuzzleFlash1.Stop();
+        MuzzleFlash2.Stop();
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            GameObject obj = Instantiate(BulletPrefab, Location1.transform.position, transform.rotation);
-            obj.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0, 0, Bulletspeed));
-        }
+           
 
-        if (Physics.Raycast(transform.position, transform.forward,out hit, 100))
-        {
-            Debug.Log("Hit!");
+
+            
+            Shoot();
         }
     }
+
+    private void Shoot()
+    {
+        MuzzleFlash1.Play();
+        MuzzleFlash2.Play();
+
+        Bullet1 = Instantiate(BulletPrefab, Location1.transform.position, transform.rotation);
+
+        Bullet1.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0, 0, Bulletspeed));
+
+        Bullet2 = Instantiate(BulletPrefab, Location2.transform.position, transform.rotation);
+
+        Bullet2.GetComponent<Rigidbody>().velocity = transform.TransformDirection(new Vector3(0, 0, Bulletspeed));
+
+        if (Physics.Raycast(Location1.transform.position, Location1.transform.forward, out hit, Range))
+        {
+            Enemy enemy = hit.transform.GetComponent<Enemy>();
+
+            if (enemy != null)
+            {
+                enemy.TakeDamage(Damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+
+            }
+            Debug.Log("Hit!");
+        }
+        Destroy(Bullet1, 1);
+        Destroy(Bullet2, 1);
+    }
+
 }
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Health = 50f;
+
+
+    public void TakeDamage(float amount)
+    {
+        Health -= amount;
+
+        if (Health <= 0f)
+        {
+            Die();
+        }
+    }
+   
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
